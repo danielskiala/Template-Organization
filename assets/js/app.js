a02ed1816d0b847eabf9d62632726c74e2b69ecc
Daniels Kiala <danielskiala@gmail.com>
2022-09-14 08:07:59 +0200

    Partie client terminer

---
+++ assets/js/app.js
@@ -0,0 +1,49 @@
+const nav = document.querySelector('.nav');
+const btnUp = document.querySelector('.btn-top');
+
+const toggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
+const overlay = document.querySelector('.nav-overlay');
+const cancel  = document.querySelector('.cancel');
+
+const links = document.querySelectorAll('.nav-links a');
+const activePage = window.location.pathname;
+const onScroll = function(e){
+    const scroll = window.scrollY;
+    if(scroll > 150){
+        nav.classList.add('background');
+    }else{
+        nav.classList.remove('background');
+    }
+    
+    if(scroll > 800){
+        btnUp.classList.add('show');
+    }else{
+        btnUp.classList.remove('show');
+    }
+}
+
+for(var i = 0; i < links.length; i++){
+    if(links[i].href.includes(activePage)){
+        links[i].classList.add('active');
+    }
+}
+
+toggle.addEventListener('click', function(){
+    navLinks.classList.add('active');
+})
+
+cancel.addEventListener('click', function(){
+    navLinks.classList.remove('active');
+})
+
+overlay.addEventListener('click', function(){
+    navLinks.classList.remove('active');
+})
+
+
+
+btnUp.addEventListener('click', function(){
+    document.documentElement.scrollTop = 0;
+});
+window.addEventListener('scroll', onScroll);
--- assets/js/swiperjs/carousel.js
+++ assets/js/swiperjs/carousel.js
@@ -1,6 +1,11 @@
+let speed = 4500;
+let speed_3 = 2000;
 var swiper = new Swiper(".swiper-carousel", {
     loop:true,
-    autoplay:true,
+    autoplay:{
+      delay:speed,
+      disableOnInteraction:false
+    },
     centerSlide: 'true',
     fade:'true',
     grabCursor:'true',
@@ -9,6 +14,7 @@ var swiper = new Swiper(".swiper-carousel", {
       prevEl: ".swiper-button-prev",
     },
   });
+
 
   var swiper_2 = new Swiper(".swiper-actu", {
     loop:true,
@@ -22,11 +28,25 @@ var swiper = new Swiper(".swiper-carousel", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints : {
+      0:{
+        slidesPerView : 1
+      },
+      520:{
+        slidesPerView : 2
+      },
+      950:{
+        slidesPerView : 3
+      }
+    }
   });
 
   var swiper_3 = new Swiper(".swiper-team", {
     loop:true,
-    autoplay:true,
+    autoplay:{
+      delay:speed_3,
+      disableOnInteraction: false
+    },
     centerSlide: 'true',
     spaceBetween:15,
     slidesPerView: 3,
@@ -36,5 +56,47 @@ var swiper = new Swiper(".swiper-carousel", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    breakpoints : {
+      0:{
+        slidesPerView : 1
+      },
+      520:{
+        slidesPerView : 2
+      },
+      950:{
+        slidesPerView : 3
+      }
+    }
   });
+
+  var swiper_4 = new Swiper(".swiper-partenair", {
+    loop:true,
+    autoplay:true,
+    centerSlide: 'true',
+    spaceBetween:20,
+    slidesPerView: 5,
+    fade:'true',
+    grabCursor:'true',
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints : {
+      0:{
+        slidesPerView : 2
+      },
+      520:{
+        slidesPerView : 3
+      },
+      950:{
+        slidesPerView : 4
+      }
+    }
+  });
+
+  function change_speed(speed, speed_3){
+    swiper.params.autoplay.delay = speed;
+    swiper_3.params.autoplay.delay = speed_3;
+  }
+
  
